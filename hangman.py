b07368c2d0cf3d239d2b3c630cee860279790b15
Jayneel Vora <jayneelv68@gmail.com>
2018-07-25 10:26:30 +0530

    final update
    final update.

--- hangman.py
+++ hangman.py
@@ -6,9 +6,10 @@ Created on Wed Jul 18 09:46:58 2018
 @author: jayneel
 """
 
-def hangman(word):
+def hangman(word,diff):
     word=word.upper()
     solved=0
+    score=diff
     #remove consonants
     word2=[]
     for x in word:
@@ -27,32 +28,32 @@ def hangman(word):
     corr=0
     maxtrial=10
     wrongno=0
+    
     while wrongno<maxtrial and solved==0:
         if trialno>0:
             
             print("No of trials:"+str(trialno))
             print("Guesses left:"+str(maxtrial-wrongno))
             print("Misses:"+str(wrongno))
-            print("Hits:"+str(trialno-wrongno))
+            print("Hits:"+str(corr))
             print("Guesses Made:")
             print(guesses)
             
         print(word1)
+        
         guess=input("Make your guess").upper()
         hit=0
         if guess not in guesses and guess not in  ["A","E","I","O","U"] and len(guess)==1 and guess.isalpha():
-            
+            hit=0
             for x in range(0,len(word1)):
                 if word[x]==guess:
                     word1[x]=guess
                     hit=1
-            
+                    #print("HIT UPDATED"+word1[x]+"==="+guess)
                 
             trialno=trialno+1
             guesses.append(guess)
-            corr=corr+1
-            print("Hit")
-        
+            
         else:
             if guess.isalpha()==False:
                 print("Abeh, character dheela hai")
@@ -62,13 +63,22 @@ def hangman(word):
                 print("Vowels kyu dal raha hai bey!")
             else:
                 print("You have already tried that! why give off a  try!")
+        if hit==1:
+                corr=corr+1
+                #print("Hit")
+        else:
+                wrongno=wrongno+1
+            #print(str(wrongno))
+        
         if "_" not in word1:
                     solved=1
-    if hit==0:
-        wrongno+=1
-        print("Nope,not there")
-    else:
-        print("Hit")
+        if hit==1:
+            print("Hit")
+            
+        else:
+            
+            print("Nope,not there")
+            
         
     if solved==1:
         print("Yeaaay, solved! You cracked the word:"+word) 
@@ -80,4 +90,5 @@ def hangman(word):
         print(word1)
         print("Guesses made were")
         print(guesses)
-    return trialno,corr,solved
+    score=0
+    return trialno,corr,solved,score
--- trial1.py
+++ trial1.py
@@ -8,42 +8,44 @@ Created on Wed Jul 18 09:30:14 2018
 from hangman import hangman
 from beforegame import getword
 #take word randomly from database
-word,ngames,nsolved,nmoves,nhits,diff,z=getword()
 
 
 #Database Schema: Word Noofgames Noofsolved Noofmoveswhensolved difficulty
 
 """TRAINGING"""
-
-#get word
-
-#play hangman with the word
-#nooftrials,correct hits,solved
-ntrials,corr,solved=hangman(word)
-print(solved)
-#resolving parameters
-nhits+=corr
-#difficulty update
-diff=(diff*ngames+corr/ntrials)/(ngames+1)
-print(diff)
-ngames+=1
-
-if solved==1:
-    nsolved+=1
-    nmoves+=ntrials
-else:
-    nmoves+=10
+def training():
+    word,ngames,nsolved,nmoves,nhits,diff,z=getword()
+    ntrials,corr,solved,score=hangman(word,z)
+    print(solved)
+    print(score)
+    #resolving parameters
+    nhits+=corr
+    #difficulty update
+    diff=(diff*ngames+corr/ntrials)/(ngames+1)
+    print(diff)
+    ngames+=1
+    
+    if solved==1:
+        nsolved+=1
+        nmoves+=ntrials
+    else:
+        nmoves+=10
 
 
     
-stri=word+":"+str(ngames)+":"+str(nsolved)+":"+str(nmoves)+":"+str(diff)+":\n"
+    stri=word+":"+str(ngames)+":"+str(nsolved)+":"+str(nmoves)+":"+str(diff)+":\n"
 
 
-s = open("words1.txt").read()
-s = s.replace(z,stri)
-f = open("words1.txt", 'w')
-f.write(s)
-f.close()
+    s = open("words1.txt").read()
+    s = s.replace(z,stri)
+    f = open("words1.txt", 'w')
+    f.write(s)
+    f.close()
+
+    
+#get word
+#play hangman with the word
+#nooftrials,correct hits,solved
 
 #write appropriate result to database
 
@@ -56,6 +58,110 @@ f.close()
 """Testing"""
 
 #get id of user
+def getlastid():
+    file=open("user.txt","r")
+    lastid=0
+    for x in file:
+        y=x.split(":")
+        if y[0].isalnum()==True:
+            lastid=int(float(y[0]))
+    return lastid
+    return""
+def register():
+    username=input("please enter your name")
+    file=open("user.txt","a")
+    userid=getlastid()
+    stri="\n"+str(userid+1)+":"+str(0)+":"+str(0)+":"+str(0)+":"+username+":"
+    file.write(stri)
+    file.close()
+    print("remember your id:"+str(userid+1))
+    return userid+1
+def main():
+    print("do you have an id (yes/no)?")
+    y=input().lower()
+    if y=="yes":
+        userid=int(float(input("Please enter your user id.")))
+        play(userid)
+    else:
+        print("would you like to register?")
+        y=input().lower()
+        if y=="yes":
+            userid=register()
+            play(userid)
+        else:
+            print("i am sorry, you need to register. Please restart the app.")
+        
+def play(userid):
+    
+    file=open("user.txt","r")
+    userlevel=0
+    strrep=""
+    userscore=0
+    for x in file:
+        user=x.split(":")
+        print(user)
+        if int(float(user[0]))==userid:
+            strrep=x
+            ngamees=int(float(user[1]))
+            userlevel=int(float(user[2]))
+            userscore=int(float(user[3]))
+            username=user[4]
+#trialno,corr,solved,score
+    print("Hi! "+username+", let us start the game!")
+    word,ngames,nsolved,nmoves,nhits,diff,z=getword()
+    ntrials,corr,solved,score=hangman(word,z)
+    nhits+=corr
+    #difficulty update
+    diff=(diff*ngames+corr/ntrials)/(ngames+1)
+    ngames+=1
+    
+    if solved==1:
+        nsolved+=1
+        nmoves+=ntrials
+    else:
+        nmoves+=10
+
+    userscore=(diff*ngames+userscore)/(userscore+1)
+    if userscore<0.25:
+        userlevel=0
+    elif userscore<0.5:
+        userlevel=1
+    elif userscore<0.75:
+        userlevel=2
+    elif userscore<1:
+        userlevel=3
+    else:
+        userlevel=4
+        
+    nhits+=corr
+    #difficulty update
+    diff=(diff*ngames+corr/ntrials)/(ngames+1)
+    print(diff)
+    ngames+=1
+    
+    if solved==1:
+        nsolved+=1
+        nmoves+=ntrials
+    else:
+        nmoves+=10
+
+
+    
+    stri=word+":"+str(ngames)+":"+str(nsolved)+":"+str(nmoves)+":"+str(diff)+":\n"
+
+
+    s = open("words1.txt").read()
+    s = s.replace(z,stri)
+    f = open("words1.txt", 'w')
+    f.write(s)
+    f.close()
+    
+    stri=str(userid)+":"+str(ngamees)+":"+str(userlevel)+":"+str(userscore)+":"+username+":"
+    s = open("user.txt").read()
+    s = s.replace(strrep,stri)
+    f = open("user.txt", 'w')
+    f.write(s)
+    f.close()
 #fetch user level, name using ID
 #based on the level, fetch a level.
 
@@ -66,3 +172,5 @@ f.close()
 #get results, and log the game details, and user level appropriately.
 #write appropriate result to database
 
+"""USAGE"""
+main()
